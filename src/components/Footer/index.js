17397b4ef633bc1c7b583954f813df574b869076
Ivan Nefagin <dfyz0r@list.ru>
2020-10-24 11:06:57 +0300

    feat: add Footer component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,8 +2,25 @@ import React from 'react';
 
 import './index.css';
 
-const Footer = () => {
+import Copyright from '../Copyright';
 
-};
+const Footer = () => (
+  <footer className="page-footer">
+    <div className="page-footer__container container">
+      <div className="page-footer__copyright">
+        <Copyright />
+      </div>
+      <p className="page-footer__developer">
+        Сделано
+        {' '}
+        <a className="page-footer__developer-link" href="https://greatstudentkaze.ru">"ВАШЕ ИМЯ"</a>
+      </p>
+      <p className="page-footer__contact">
+        <a className="page-footer__tel" href="tel:+79999991234">+7 (999) 999 - 1234</a>
+        <a className="page-footer__contact-link" href="/">Перезвоните мне</a>
+      </p>
+    </div>
+  </footer>
+);
 
 export default Footer;
