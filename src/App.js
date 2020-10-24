c480689b57a48d8d72c439b650d68844bcd62fe3
Ivan Nefagin <dfyz0r@list.ru>
2020-10-24 14:14:12 +0300

    feat(App.js): import Contact component

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import './css/fonts.css';
 import './css/index.css';
 
 import Advantages from './components/Advantages';
+import Contact from './components/Contact';
 import Footer from './components/Footer';
 
 const App = () => {
   return (
     <>
       <Advantages />
+      <Contact />
       <Footer />
     </>
   );
