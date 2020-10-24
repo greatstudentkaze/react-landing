9cef31a2229458525eae8898ae54cae4e226dcca
Ivan Nefagin <dfyz0r@list.ru>
2020-10-24 11:05:41 +0300

    feat: add Copyright component

---
+++ src/components/Copyright/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './index.css';
+import Logo from '../Logo';
+
+const Copyright = () => (
+  <div className="copyright">
+    <Logo />
+    <p className="copyright__text">
+      © 2020 XXXcompany.
+      Все права защищены.
+    </p>
+  </div>
+);
+
+export default Copyright;
