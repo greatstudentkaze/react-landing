3fa8d724272dda18e0f2b60112e8838a08c12ef1
Ivan Nefagin <dfyz0r@list.ru>
2020-10-24 15:39:55 +0300

    feat(Header): add Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './index.css';
+import Logo from '../Logo';
+import Phone from '../Phone';
+
+const Header = () => (
+  <header className="page-header">
+    <Logo />
+    <Phone />
+  </header>
+);
+
+export default Header;
