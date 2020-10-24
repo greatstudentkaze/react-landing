c4d8908da2e450eae9b385e248a0425afeb5af97
Ivan Nefagin <dfyz0r@list.ru>
2020-10-24 15:40:13 +0300

    feat(App.js): import Promo component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './css/normalize.css';
 import './css/fonts.css';
 import './css/index.css';
 
+import Promo from './components/Promo';
 import Advantages from './components/Advantages';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
@@ -11,6 +12,7 @@ import Footer from './components/Footer';
 const App = () => {
   return (
     <>
+      <Promo />
       <Advantages />
       <Contact />
       <Footer />
