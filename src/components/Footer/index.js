06429f7e03d9938e8ce9447654ae046a994a530d
Ivan Nefagin <dfyz0r@list.ru>
2020-10-24 15:51:16 +0300

    feat(Footer): import Phone component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './index.css';
 
 import Copyright from '../Copyright';
+import Phone from '../Phone';
 
 const Footer = () => (
   <footer className="page-footer">
@@ -11,14 +12,9 @@ const Footer = () => (
         <Copyright />
       </div>
       <p className="page-footer__developer">
-        Сделано
-        {' '}
-        <a className="page-footer__developer-link" href="https://greatstudentkaze.ru">"ВАШЕ ИМЯ"</a>
+        Сделано <a className="page-footer__developer-link" href="https://greatstudentkaze.ru">"ВАШЕ ИМЯ"</a>
       </p>
-      <p className="page-footer__contact">
-        <a className="page-footer__tel" href="tel:+79999991234">+7 (999) 999 - 1234</a>
-        <a className="page-footer__contact-link" href="/">Перезвоните мне</a>
-      </p>
+      <Phone />
     </div>
   </footer>
 );
